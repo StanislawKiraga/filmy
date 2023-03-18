813d9b8b41b2d2b015ecbddf543a2174e124f62d
StanislawKiraga <kiragasta@gmail.com>
2023-03-18 10:17:40 +0100

    ...

--- filmy.py
+++ filmy.py
@@ -1,17 +1,13 @@
 from faker import Faker
 fake = Faker()
-
+import datetime
+import random
 
 class Films:
     def __init__(self, title, year, movie_type):
         self.title = title
         self.year = year
         self.movie_type = movie_type
-
-        self.views = 0
-    
-    def play(self, step=1):
-        self.views += step
     
     def __str__(self):
         return f'{self.title}, {self.year}'
@@ -24,9 +20,6 @@ class Series(Films):
         super().__init__(*args, **kwargs)
         self.season_number = season_number
         self.episod_number = episod_number
-
-    def play(self, step=1):
-        self.views += step
     
     def __str__(self):
         return f'{self.title}, S{self.season_number:02}E{self.episod_number:02}'
@@ -34,6 +27,21 @@ class Series(Films):
     def __repr__(self):
         return f'Serial:[{self.title}, S{self.season_number:02}E{self.episod_number:02}]'
     
+
+film1 = Films(title='Gladiator', year=2000, movie_type='action')
+film2 = Films(title='Red Roses', year=1994, movie_type='dramat')
+film3 = Films(title='Zukazama', year=2020, movie_type='family')
+film4 = Films(title='Lion king', year=2002, movie_type='animation')
+film5 = Films(title='Saw', year=2005, movie_type='horror')
+
+serial1 = Series(title='The office', year=1999, movie_type='paradocument', season_number=4, episod_number=8)
+serial2 = Series(title='Breaking Bad', year=1999, movie_type='action', season_number=2, episod_number=7)
+serial3 = Series(title='Paw patrol', year=1999, movie_type='scifi', season_number=1, episod_number=4)
+serial4 = Series(title='Lost', year=1999, movie_type='thriller', season_number=5, episod_number=5)
+serial5 = Series(title='Świat według kiepskich', year=1999, movie_type='comedy', season_number=3, episod_number=9)
+
+films_list = [film1, film2, film3, film4, film5, serial1, serial2, serial3, serial4, serial5]
+
 def get_movies(list):
     only_movies = []
     for movie in list:
@@ -55,19 +63,11 @@ def search(x):
         if movie.title == x:
             print(movie)
 
-film1 = Films(title='Gladiator', year=2000, movie_type='action')
-film2 = Films(title='Red Roses', year=1994, movie_type='dramat')
-film3 = Films(title='Zukazama', year=2020, movie_type='family')
-film4 = Films(title='Lion king', year=2002, movie_type='animation')
-film5 = Films(title='Saw', year=2005, movie_type='horror')
+def generate_views(y=random.randint(1, 100)):
+    return f'{random.choice(films_list)}, Views:{y}'
+    
 
-serial1 = Series(title='The office', year=1999, movie_type='paradocument', season_number=4, episod_number=8)
-serial2 = Series(title='Breaking Bad', year=1999, movie_type='action', season_number=2, episod_number=7)
-serial3 = Series(title='Paw patrol', year=1999, movie_type='scifi', season_number=1, episod_number=4)
-serial4 = Series(title='Lost', year=1999, movie_type='thriller', season_number=5, episod_number=5)
-serial5 = Series(title='Świat według kiepskich', year=1999, movie_type='comedy', season_number=3, episod_number=9)
 
-films_list = [film1, film2, film3, film4, film5, serial1, serial2, serial3, serial4, serial5]
+print(generate_views())
 
-search('Breaking Bad')
-
+    
