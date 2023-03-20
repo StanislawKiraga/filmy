64154c23f7ff497523a7c80fec541a93b3579d21
StanislawKiraga <kiragasta@gmail.com>
2023-03-20 11:58:43 +0100

    ...

--- filmy.py
+++ filmy.py
@@ -4,7 +4,7 @@ from datetime import date
 import random
 from functools import wraps
 
-class Films:
+class Film:
     def __init__(self, title, year, movie_type):
         self.title = title
         self.year = year
@@ -22,7 +22,7 @@ class Films:
     def __repr__(self):
         return f'[{self.title}, year:{self.year}, type:{self.movie_type}, view:{self.nr_view}]'
     
-class Series(Films):
+class Serie(Film):
     def __init__(self, season_number, episod_number, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.season_number = season_number
@@ -35,24 +35,24 @@ class Series(Films):
         return f'Serial:[{self.title}, S{self.season_number:02}E{self.episod_number:02}, view:{self.nr_view}]'
     
 
-film1 = Films(title='Gladiator', year=2000, movie_type='action')
-film2 = Films(title='Red Roses', year=1994, movie_type='dramat')
-film3 = Films(title='Zukazama', year=2020, movie_type='family')
-film4 = Films(title='Lion king', year=2002, movie_type='animation')
-film5 = Films(title='Saw', year=2005, movie_type='horror')
+film1 = Film(title='Gladiator', year=2000, movie_type='action')
+film2 = Film(title='Red Roses', year=1994, movie_type='dramat')
+film3 = Film(title='Zukazama', year=2020, movie_type='family')
+film4 = Film(title='Lion king', year=2002, movie_type='animation')
+film5 = Film(title='Saw', year=2005, movie_type='horror')
 
-serial1 = Series(title='The office', year=1999, movie_type='paradocument', season_number=4, episod_number=8)
-serial2 = Series(title='Breaking Bad', year=1999, movie_type='action', season_number=2, episod_number=7)
-serial3 = Series(title='Paw patrol', year=1999, movie_type='scifi', season_number=1, episod_number=4)
-serial4 = Series(title='Lost', year=1999, movie_type='thriller', season_number=5, episod_number=5)
-serial5 = Series(title='Świat według kiepskich', year=1999, movie_type='comedy', season_number=3, episod_number=9)
+serial1 = Serie(title='The office', year=1999, movie_type='paradocument', season_number=4, episod_number=8)
+serial2 = Serie(title='Breaking Bad', year=1999, movie_type='action', season_number=2, episod_number=7)
+serial3 = Serie(title='Paw patrol', year=1999, movie_type='scifi', season_number=1, episod_number=4)
+serial4 = Serie(title='Lost', year=1999, movie_type='thriller', season_number=5, episod_number=5)
+serial5 = Serie(title='Świat według kiepskich', year=1999, movie_type='comedy', season_number=3, episod_number=9)
 
 films_list = [film1, film2, film3, film4, film5, serial1, serial2, serial3, serial4, serial5]
 
 def get_movies():
     only_movies = []
     for movie in films_list:
-        if not isinstance(movie, Series):
+        if not isinstance(movie, Serie):
             only_movies.append(movie)    
             only_movies = sorted(only_movies, key=lambda movie: movie.title)       
     return only_movies
@@ -60,7 +60,7 @@ def get_movies():
 def get_series(list):
     only_series = []
     for movie in list:
-        if isinstance(movie, Series):
+        if isinstance(movie, Serie):
             only_series.append(movie)
             only_series = sorted(only_series, key=lambda movie: movie.title)
     return only_series
